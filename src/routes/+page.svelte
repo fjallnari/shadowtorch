964877e12b889cb27cabc480bee024f1d3e2f4a6
ashmogh <draconirx@gmail.com>
2023-04-11 20:48:20 +0200

    Init sveltekit with tailwind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-3xl font-bold underline text-stone-50">
+    Hello world!
+</h1>
+
+<style lang="postcss">
+
+    :global(html) {
+        background-color: theme(colors.gray.950);
+    }
+</style>
