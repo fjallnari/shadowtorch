52c58672bea5ee4d809432683e47d42e2e57e971
ashmogh <draconirx@gmail.com>
2023-05-06 00:40:49 +0200

    Test pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA()
+	]
 });
