f6967d8aebdca781788cb61394b91a894302bfab
fjallnari <draconirx@gmail.com>
2024-02-18 09:28:55 +0100

    Uppercase app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-	appId: 'app.vercel.shadowtorch',
-	appName: 'shadowtorch',
+	appId: 'shadowtorch',
+	appName: 'Shadowtorch',
 	webDir: 'build',
 	server: {
 		androidScheme: 'https'
