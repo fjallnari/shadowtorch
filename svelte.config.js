775b08b522c69cbef12eab9220be8b9df0aff095
fjallnari <draconirx@gmail.com>
2025-03-25 21:39:17 +0100

    Add Dockerfile & node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
