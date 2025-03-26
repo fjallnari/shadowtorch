542aaf20754b8c682f53744e33d1bef97b5e7c1b
fjallnari <fjallnari@gmail.com>
2025-03-26 14:58:23 +0100

    Add android specific static fallbacks

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 //import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+//?(ANDROID) use adapter-static to generate mobile apps
 import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
